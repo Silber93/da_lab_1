04e55a305e685a070a5ea9c2df3abcfacfd500c0
Silber93 <silber14.mailbox@gmail.com>
2020-10-31 21:03:35 +0200

    added data_reader.py that converts the .txt file to .csv and saves it.

---
+++ data_reader.py
@@ -0,0 +1,68 @@
+import pandas as pd
+from datetime import datetime as dt
+
+TEXT_TO_VAL = [
+    {'name': 'id', 'start': '$oid":"', 'end': '"', 'type': str},
+    {'name': 'delay', 'start': '"delay":', 'end': ',', 'type': float},
+    {'name': 'congestion', 'start': '"congestion":', 'end': ',', 'type': bool},
+    {'name': 'lineId', 'start': '"lineId":"', 'end': '"', 'type': str},
+    {'name': 'vehicleId', 'start': '"vehicleId":', 'end': ',', 'type': str},
+    {'name': 'timestamp', 'start': '"$numberLong":"', 'end': '"', 'type': float},  # TODO: Change type to dt.timestamp
+    {'name': 'areaId', 'start': '"areaId":', 'end': ',', 'type': str},
+    {'name': 'areaId1', 'start': '"areaId1":', 'end': ',', 'type': str},
+    {'name': 'areaId2', 'start': '"areaId2":', 'end': ',', 'type': str},
+    {'name': 'areaId3', 'start': '"areaId3":', 'end': ',', 'type': str},
+    {'name': 'gridID', 'start': '"gridID":"', 'end': '"', 'type': str},
+    {'name': 'actualDelay', 'start': '"actualDelay":', 'end': ',', 'type': float},
+    {'name': 'longitude', 'start': '"longitude":', 'end': ',', 'type': float},
+    {'name': 'latitude', 'start': '"latitude":', 'end': ',', 'type': float},
+    {'name': 'currentHour', 'start': '"currentHour":', 'end': ',', 'type': float},
+    {'name': 'dateTypeEnum', 'start': '"dateTypeEnum":"', 'end': '"', 'type': str},
+    {'name': 'angle', 'start': '"angle":', 'end': ',', 'type': float},
+    {'name': 'ellapsedTime', 'start': '"ellapsedTime":', 'end': ',', 'type': float},
+    {'name': 'vehicleSpeed', 'start': '"vehicleSpeed":', 'end': ',', 'type': float},
+    {'name': 'distanceCovered', 'start': '"distanceCovered":', 'end': ',', 'type': float},
+    {'name': 'journeyPatternId', 'start': '"journeyPatternId":"', 'end': '"', 'type': str},
+    {'name': 'direction', 'start': '"direction":', 'end': ',', 'type': float},
+    {'name': 'busStop', 'start': '"busStop":', 'end': ',', 'type': str},  # TODO: Check if type isn't only float/int
+    {'name': 'poiId', 'start': '"poiId":', 'end': ',', 'type': str},
+    {'name': 'poiId2', 'start': '"poiId2":', 'end': ',', 'type': str},
+    {'name': 'systemTimestamp', 'start': '"systemTimestamp":', 'end': ',', 'type': float},
+    {'name': 'calendar', 'start': '"$numberLong":"', 'end': '"', 'type': float},  # TODO: Change type to dt.timestamp
+    {'name': 'filteredActualDelay', 'start': '"filteredActualDelay":', 'end': ',', 'type': float},
+    {'name': 'atStop', 'start': '"atStop":', 'end': ',', 'type': bool},
+    {'name': 'dateType', 'start': '"dateType":', 'end': ',', 'type': str},  # TODO: Check if type is actually float
+    {'name': 'justStopped', 'start': '"justStopped":', 'end': ',', 'type': bool},
+    {'name': 'justLeftStop', 'start': '"justLeftStop":', 'end': ',', 'type': bool},
+    {'name': 'probability', 'start': '"probability":', 'end': ',', 'type': float},
+    {'name': 'anomaly', 'start': '"anomaly":', 'end': ',', 'type': bool},
+    {'name': 'loc_type', 'start': '"loc":{"type":"', 'end': '"', 'type': str},
+    # {'name': 'coordinates', 'start': '"coordinates":', 'end': '}', 'type': list}, TODO: Check if necessary
+]
+
+def txt_to_dataframe(filename, num_rows):
+    all_data_dict = {}
+    for key in TEXT_TO_VAL:
+        all_data_dict[key['name']] = []
+    with open(filename) as f:
+        for i, line in enumerate(f):
+            if i == num_rows:
+                break
+            print(i, line[:-1])  # last char in original line is '\n'
+            for key in TEXT_TO_VAL:
+                value = line.split(key['start'])[1].split(key['end'])[0]
+                value = key['type'](value)
+                # print(f"\t {key['name']}: {value} ({type(value)})")
+                all_data_dict[key['name']].append(value)
+    df = pd.DataFrame.from_dict(all_data_dict).set_index('id')
+    return df
+
+
+
+
+file = "sampled_200.txt"
+rows_to_show = 200
+
+df = txt_to_dataframe(file, rows_to_show)
+print(df)
+df.to_csv('sampled_' + str(rows_to_show) + '.csv')
