272dd9b3defab4079eba94efcd4e774c85b7807e
Silber93 <silber14.mailbox@gmail.com>
2020-11-05 12:42:46 +0200

    1) now data_reader can extract data by selection. 2) timpestamps converted to dates.

--- data_reader.py
+++ data_reader.py
@@ -49,8 +49,7 @@ def txt_to_dataframe(filename, condition):
         for i, line in enumerate(f):
             if i % 100000 == 0:
                 td = (dt.now() - tick).total_seconds()
-                print(f"{i / 10000}*10^5, {len(all_data_dict['id'])}, {round(td, 3)} sec, "
-                      f"pace: {round(i+1 / td, 3)} rows/sec")
+                print(f"{i / 10000}*10^5, {len(all_data_dict['id'])}, {round(td, 3)} sec, pace: {round(i+1 / td, 3)} rows/sec")
             if condition['name'] == 'num_rows' and i == condition['value']:
                 break
             key = [x for x in TEXT_TO_VAL if x['name'] == condition['name']]
