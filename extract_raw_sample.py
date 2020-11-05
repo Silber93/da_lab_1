a63362c847701890606212ee844df91b8ebd0c2b
Silber93 <silber14.mailbox@gmail.com>
2020-11-05 18:58:22 +0200

    1) now data_reader can extract data by selection. 2) timpestamps converted to dates.

--- extract_raw_sample.py
+++ extract_raw_sample.py
@@ -3,14 +3,17 @@ import random
 FILE_MAIN = '/datashare/busFile'
 SIZE = 237143372
 
-proportion = float(1 / 1000)
-print(proportion)
+proportion = 1 / float(1000)
+
 
 
 sample_size = int(SIZE * proportion)
 print(sample_size)
 
 print("starting")
+
+sampled_idx = sorted(random.sample(range(SIZE), sample_size))
+print(sampled_idx)
 
 sampled_lines = []
 j = 0
@@ -19,8 +22,9 @@ with open(FILE_MAIN) as f:
     for i, line in enumerate(f):
         if i % 1000000 == 0:
             print(round(100 * (i / SIZE), 3), '%')
-        if random.random() <= proportion:
+        if i == sampled_idx[j]:
             sampled_lines.append(line)
+            j += 1
 
 with open('sampled_random.txt', 'w') as f:
     for i, line in enumerate(sampled_lines):
